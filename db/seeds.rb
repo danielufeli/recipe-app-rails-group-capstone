abc95b9f236a9da414925d82799695670955f82b
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-31 19:21:10 -0500

    Add test data to the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+u1 = User.create(name: 'user1', email: 'user1@gmail.com', password: 'aaaaaa', confirmed_at: Time.now)
+u2 = User.create(name: 'user2', email: 'user2@gmail.com', password: 'aaaaaa', confirmed_at: Time.now)
+
+food1 = Food.create(name: 'food1', measurement_unit: 'kile', price: 2.5, user: u1)
+food2 = Food.create(name: 'food2', measurement_unit: 'kile', price: 5.3, user: u1)
+food3 = Food.create(name: 'food3', measurement_unit: 'kile', price: 3.0, user: u2)
+
+rec1 = Recipe.create(name: 'recipe1', preparation_time: 1.5, cooking_time: 0.75, description: 'Recipe 1 steps', user: u1)
+rec2 = Recipe.create(name: 'recipe2', preparation_time: 2, cooking_time: 1, description: 'Recipe 2 steps', user: u1)
+
+rf1 = RecipeFood.create(quantity: 2, recipe: rec1, food: food1)
+rf2 = RecipeFood.create(quantity: 1, recipe: rec1, food: food2)
