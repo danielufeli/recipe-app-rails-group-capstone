8f48cdca56d5699a62e8e06c7b27e6ca40c81442
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-31 19:42:46 -0500

    Create the Recipes Controller with index, new, create, and destroy actions

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,34 @@
+class RecipesController < ApplicationController
+  def index
+    @current_user = current_user
+    @recipes = @current_user.recipes.order(created_at: :desc)
+  end
+
+  def new
+    @current_user = current_user
+  end
+
+  def create
+    @current_user = current_user
+    @recipe = @current_user.recipes.new(recipe_params)
+    if @recipe.save
+      flash[:alert] = 'Recipe created successfully'
+      redirect_to recipes_url
+    else
+      flash.now[:alert] = @recipe.errors.full_messages.to_sentence
+      render :new, status: 422
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe][:recipe_id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+end
