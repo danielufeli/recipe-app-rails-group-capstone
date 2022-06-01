40843f7f5c2a62f31bacbcff9dc1ad98268deb44
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-31 19:37:20 -0500

    Setup Recipe model's associations and validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,17 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id', inverse_of: :recipes
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id', inverse_of: :recipe, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 300 }
+  validates :preparation_time, numericality: true, comparison: { greater_than: 0 }
+  validates :cooking_time, numericality: true, comparison: { greater_than: 0 }
+  validates :description, presence: true
+
+  after_initialize :default_values
+
+  private
+
+  def default_values
+    self.public ||= false
+  end
 end
