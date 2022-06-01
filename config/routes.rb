ec9869270e963ee0dd4d89656ceae69f1c303315
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-01 16:40:49 -0500

    Add recipe_foods index, new, edit, and destroy paths to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :foods, only: %i[index]
   resources :recipes, only: %i[index new create show destroy]
+  resources :recipe_foods, only: %i[index new edit destroy]
 
   # Defines the root path route ("/")
   root "recipes#index"
