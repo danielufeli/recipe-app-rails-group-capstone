3dc670e54ba42affacce749a1bff2c7614613306
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-01 16:39:45 -0500

    Add recipe show path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :foods, only: %i[index]
-  resources :recipes, only: %i[index new create destroy]
+  resources :recipes, only: %i[index new create show destroy]
 
   # Defines the root path route ("/")
   root "recipes#index"
