0f2dd95ede65ab8a52de580c8488f977b34d4994
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-01 19:49:39 -0500

    Update the index controller action to fetch only public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
     @current_user = current_user
-
-    @public_recipes = Recipe.includes(:user).includes(:recipe_foods).order(created_at: :desc)
+    @public_recipes = Recipe.includes(:user).includes(:recipe_foods).where(public: true).order(created_at: :desc)
   end
 end
