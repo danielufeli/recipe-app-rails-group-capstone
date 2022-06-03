72d79c9680728b976d9d3ea8241764b307cadfc7
Daniel Ufeli <danielufeli@yahoo.com>
2022-06-03 16:16:41 +0100

    Add Foods CRUD and Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :foods, only: %i[index]
+  resources :foods, only: %i[index create destroy]
   resources :recipes, only: %i[index new create show destroy]
   resources :public_recipes, only: %i[index]
   resources :recipe_foods, only: %i[index new edit update destroy]
